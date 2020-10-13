8e0457ec8977c93d2d88acce60c209f72f3a9bee
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-10-13 21:30:51 +0200

    Create and render black pieces

--- koala_chess/src/game.rs
+++ koala_chess/src/game.rs
@@ -16,43 +16,70 @@ impl Game {
 
         let mut pieces: Vec<Piece> = Vec::new();
 
-        // Create white pieces
+        // Create pieces
         for board_x in 0..8 {
             #[rustfmt::skip]
             let white_pawn = Piece::new(PieceColor::White, PieceKind::Pawn, board_x, 1, atlas_shader, aspect_ratio);
+            #[rustfmt::skip]
+            let black_pawn = Piece::new(PieceColor::Black, PieceKind::Pawn, board_x, 6, atlas_shader, aspect_ratio);
 
             pieces.push(white_pawn);
+            pieces.push(black_pawn);
         }
 
         #[rustfmt::skip]
         let left_white_rook = Piece::new(PieceColor::White, PieceKind::Rook, 0, 0, atlas_shader, aspect_ratio);
         #[rustfmt::skip]
         let right_white_rook = Piece::new(PieceColor::White, PieceKind::Rook, 7, 0, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let left_black_rook = Piece::new(PieceColor::Black, PieceKind::Rook, 0, 7, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let right_black_rook = Piece::new(PieceColor::Black, PieceKind::Rook, 7, 7, atlas_shader, aspect_ratio);
 
         #[rustfmt::skip]
         let left_white_knight = Piece::new(PieceColor::White, PieceKind::Knight, 1, 0, atlas_shader, aspect_ratio);
         #[rustfmt::skip]
         let right_white_knight = Piece::new(PieceColor::White, PieceKind::Knight, 6, 0, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let left_black_knight = Piece::new(PieceColor::Black, PieceKind::Knight, 1, 7, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let right_black_knight = Piece::new(PieceColor::Black, PieceKind::Knight, 6, 7, atlas_shader, aspect_ratio);
 
         #[rustfmt::skip]
         let left_white_bishop = Piece::new(PieceColor::White, PieceKind::Bishop, 2, 0, atlas_shader, aspect_ratio);
         #[rustfmt::skip]
         let right_white_bishop = Piece::new(PieceColor::White, PieceKind::Bishop, 5, 0, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let left_black_bishop = Piece::new(PieceColor::Black, PieceKind::Bishop, 2, 7, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let right_black_bishop = Piece::new(PieceColor::Black, PieceKind::Bishop, 5, 7, atlas_shader, aspect_ratio);
 
         #[rustfmt::skip]
         let white_queen = Piece::new(PieceColor::White, PieceKind::Queen, 3, 0, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let black_queen = Piece::new(PieceColor::Black, PieceKind::Queen, 3, 7, atlas_shader, aspect_ratio);
 
         #[rustfmt::skip]
         let white_king = Piece::new(PieceColor::White, PieceKind::King, 4, 0, atlas_shader, aspect_ratio);
+        #[rustfmt::skip]
+        let black_king = Piece::new(PieceColor::Black, PieceKind::King, 4, 7, atlas_shader, aspect_ratio);
 
         pieces.push(left_white_rook);
         pieces.push(right_white_rook);
+        pieces.push(left_black_rook);
+        pieces.push(right_black_rook);
         pieces.push(left_white_knight);
         pieces.push(right_white_knight);
+        pieces.push(left_black_knight);
+        pieces.push(right_black_knight);
         pieces.push(left_white_bishop);
         pieces.push(right_white_bishop);
+        pieces.push(left_black_bishop);
+        pieces.push(right_black_bishop);
         pieces.push(white_queen);
+        pieces.push(black_queen);
         pieces.push(white_king);
+        pieces.push(black_king);
 
         Game { board, pieces }
     }
