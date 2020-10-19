b37bd545933db5bfa66788c4b88a0c910db84e87
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-10-19 21:21:53 +0200

    Add GLX bindings

---
+++ glx/build.rs
@@ -0,0 +1,11 @@
+use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
+use std::{env, fs::File, path::Path};
+
+fn main() {
+    let dest = env::var("OUT_DIR").unwrap();
+    let mut file = File::create(&Path::new(&dest).join("bindings.rs")).unwrap();
+
+    Registry::new(Api::Glx, (1, 4), Profile::Core, Fallbacks::All, [])
+        .write_bindings(GlobalGenerator, &mut file)
+        .unwrap();
+}
